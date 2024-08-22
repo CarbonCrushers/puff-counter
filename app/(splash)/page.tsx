e99b398b2094db73a1a26df1a591aeab4ec63663
MohamedTaha91 <Taha_Acme@hotmial.com>
2024-08-22 12:54:56 +0200

    login page

--- app/(splash)/page.tsx
+++ app/(splash)/page.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function HomePage() {
   return (
-    <div className="flex grow justify-center items-center">
+    <div className="flex flex-col grow justify-center items-center">
       <h1 className="text-5xl font-bold text-center">
         Welcome to Puff Counter
       </h1>
+      <div className="flex flex-col w-1/3 items-center p-6 space-y-4 mt-32">
+        <Input
+          className="h-20 border-2 border-black shadow-md"
+          type="text"
+          placeholder="Email"
+        ></Input>
+        <Input
+          className="h-20 border-2 border-black shadow-md"
+          type="password"
+          placeholder="Password"
+        ></Input>
+        <Button className="h-20 w-full" size="default">
+          Login
+        </Button>
+      </div>
     </div>
   );
 }
